b61d7bac3e722b79a1a74a50057bfe7d83f565ba
AriadnaCastro <ac240962@fi365.ort.edu.uy>
2026-06-05 20:48:39 -0300

    Login Admin

---
+++ Frontend/src/components/loginAdmin/LoginAdmin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LoginForm from './LoginAdminForm'
+import LoginFooter from './LoginAdminFooter'
+
+const Login = () => {
+   return (
+    <main className="login-card">
+      <LoginForm />
+      <LoginFooter />
+    </main>
+  );
+}
+
+export default Login
