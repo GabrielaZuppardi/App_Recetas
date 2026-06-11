4f7ae5a22fb5836fece88acaf642386d6ccf859e
gnzm15 <gnzm.15@gmail.com>
2026-06-10 23:18:38 -0300

    agrego v1

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://app-recetas-qrrs.vercel.app/',
+    baseURL: 'https://app-recetas-qrrs.vercel.app/v1',
         
            
 
